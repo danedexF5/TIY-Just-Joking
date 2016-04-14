5c7b986bc78c8ad5dce2f98635fed81eb36ced2a
Dane Dexheimer <dane.dexheimer@gmail.com>
2016-04-13 22:09:34 -0400

    Family joke

---
+++ src/Joke.java
@@ -0,0 +1,8 @@
+/**
+ * Created by danedexheimer on 4/13/16.
+ */
+public class Joke {
+    public static void main(String[] args){
+        System.out.println("Knock off it's horns, wipe it's ass and throw it on the plate");
+    }
+}
